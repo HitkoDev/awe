2394bce313f8aca0730575a7f3fb357e5fe29c58
HitkoDev <development@hitko.si>
2021-01-17 14:23:45 +0100

    im2

--- im2.py
+++ im2.py
@@ -10,6 +10,8 @@ from dataset import AWEDataset, load_img
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
+image_size = 299
+
 train_dataset = AWEDataset(os.path.join('./images/converted', 'train'))
 test_dataset = AWEDataset(os.path.join('./images/converted', 'test'))
 
@@ -19,7 +21,7 @@ for i in range(len(train_dataset.images)):
 
 
 def train():
-    n = 25
+    n = 2
     i = 0
     a = []
     b = []
@@ -31,12 +33,11 @@ def train():
             for im in x:
                 p = im['src']
                 if p not in c:
-                    c[p] = load_img(im['src'], 160, aug=False)
-                if random.random() < 0.5:
-                    a.append(c[p])
-                    b.append(labels_map[im['class']])
+                    c[p] = load_img(im['src'], image_size, aug=False)
+                a.append(c[p])
+                b.append(labels_map[im['class']])
 
-                a.append(load_img(im['src'], 160))
+                a.append(load_img(im['src'], image_size))
                 b.append(labels_map[im['class']])
             if i == n:
                 yield (np.array(a) / 255., np.array(b))
@@ -51,27 +52,25 @@ def test():
     a = []
     b = []
     for x in test_images:
-        a.append(load_img(x['src'], 160, False))
+        a.append(load_img(x['src'], image_size, False))
         b.append(labels_map[x['class']])
     while True:
         yield (np.array(a) / 255., np.array(b))
 
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(filters=64, kernel_size=4, padding='same', activation='relu', input_shape=(160, 160, 3)),
-    tf.keras.layers.MaxPooling2D(pool_size=2),
-    tf.keras.layers.Dropout(0.3),
-    tf.keras.layers.Conv2D(filters=32, kernel_size=4, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=2),
-    tf.keras.layers.Dropout(0.3),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(256, activation=None),  # No activation on final dense layer
-    tf.keras.layers.Lambda(lambda x: tf.math.l2_normalize(x, axis=1))  # L2 normalize embeddings
-])
+model = tf.keras.applications.InceptionResNetV2(
+    include_top=False,
+    weights="imagenet",
+    input_shape=(image_size, image_size, 3),
+    pooling='max'
+)
+
+model.layers.append(tf.keras.layers.Dense(256, activation=None))
+model.layers.append(tf.keras.layers.Lambda(lambda x: tf.math.l2_normalize(x, axis=1)))
 
 # Compile the model
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(0.0001),
+    optimizer=tf.keras.optimizers.Adam(0.001),
     loss=tfa.losses.TripletSemiHardLoss()
 )
 
@@ -79,7 +78,11 @@ model.compile(
 history = model.fit(
     train(),
     epochs=300,
-    steps_per_epoch=30,
+    steps_per_epoch=100,
     validation_data=test(),
-    validation_steps=1
+    validation_steps=1,
+    callbacks=[
+        tf.keras.callbacks.ModelCheckpoint(filepath='./model/model.{epoch:02d}-{val_loss:.2f}.h5'),
+        tf.keras.callbacks.TensorBoard(log_dir='./logs')
+    ]
 )
