73f6e5e379d65ffe7f79a9fc13ffacb6bf52bcc4
HitkoDev <development@hitko.si>
2021-01-17 16:16:35 +0100

    m

--- im2.py
+++ im2.py
@@ -26,7 +26,7 @@ for i in range(len(train_dataset.images)):
 
 
 def train():
-    n = 100
+    n = 50
     i = 0
     a = []
     b = []
@@ -86,7 +86,7 @@ if FLAGS.model:
 history = model.fit(
     train(),
     epochs=300,
-    steps_per_epoch=2,
+    steps_per_epoch=4,
     validation_data=test(),
     validation_steps=1,
     initial_epoch=FLAGS.epoch,
