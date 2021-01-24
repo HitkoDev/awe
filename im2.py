d2eaa808249ef0c9e4a85ba4052d33b6ee00c0c7
HitkoDev <development@hitko.si>
2021-01-24 06:33:32 +0100

    dc

--- im2.py
+++ im2.py
@@ -125,7 +125,7 @@ model.save('m2.h5')
 # Compile the model
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
     0.0001,
-    decay_steps=50,
+    decay_steps=200,
     decay_rate=0.1,
     staircase=False
 )
