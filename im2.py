970ce921ec71710f352de6885e804eac7ceacad4
HitkoDev <development@hitko.si>
2021-01-25 02:25:29 +0100

    df

--- im2.py
+++ im2.py
@@ -4,7 +4,6 @@ import random
 import numpy as np
 import tensorflow as tf
 import tensorflow_addons as tfa
-from tensorflow import keras
 
 from dataset import AWEDataset, load_img
 from model2 import image_size, model
@@ -13,9 +12,6 @@ flags = tf.compat.v1.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('epoch', 0, 'Epoch to start from')
 flags.DEFINE_string('model', '', 'Model to restore')
-
-train1_dataset = AWEDataset(os.path.join('./images/nomask', 'train'))
-test1_dataset = AWEDataset(os.path.join('./images/nomask', 'test'))
 
 train_dataset = AWEDataset(os.path.join('./images/converted', 'train'))
 test_dataset = AWEDataset(os.path.join('./images/converted', 'test'))
@@ -42,53 +38,33 @@ class TrainEpochCallback(tf.keras.callbacks.Callback):
 
 
 def train():
-    global epoch, model
+    global epoch
     n = 10
     i = 0
     a = []
     b = []
     c = {}
     while True:
-        dataset = train_dataset
-        mask = True
-        if epoch < 150:
-            dataset = train1_dataset
-            mask = False
-        random.shuffle(dataset.images)
-        for x in dataset.images:
+        random.shuffle(train_dataset.images)
+        for x in train_dataset.images:
             random.shuffle(x)
-            img = []
-            lbl = []
-            for im in x:
+
+            if epoch < 150:
                 # Include original images in the first 1/2 of traing samples
-                if epoch < 300:
+                for im in x[0:2]:
                     p = im['src']
                     if p not in c:
-                        c[p] = load_img(im['src'], image_size, mask=mask, aug=False)
-                    img.append(c[p])
-                    lbl.append(labels_map[im['class']])
+                        c[p] = load_img(im['src'], image_size, aug=False)
+                    a.append(c[p])
+                    b.append(labels_map[im['class']])
 
-                img.append(load_img(im['src'], image_size, mask=mask))
-                lbl.append(labels_map[im['class']])
-            # select pairs with greatest distance
-            pred = model.predict(np.array(img))
-            m2 = []
-            for i1 in range(len(pred) - 1):
-                for i2 in range(i1 + 1, len(pred)):
-                    dist = (np.sum((pred[i1] - pred[i2])**2))**0.5
-                    m2.append([i1, i2, dist])
-            m2 = sorted(m2, key=lambda a_entry: -a_entry[2])
-            a.append(img[m2[0][0]])
-            b.append(lbl[m2[0][0]])
-            a.append(img[m2[0][1]])
-            b.append(lbl[m2[0][1]])
-            for en in m2[1:]:
-                if en[0] != m2[0][0] and en[1] != m2[0][0] and en[0] != m2[0][1] and en[1] != m2[0][1]:
-                    a.append(img[en[0]])
-                    b.append(lbl[en[0]])
-                    a.append(img[en[1]])
-                    b.append(lbl[en[1]])
-                    break
+                for im in x[2:4]:
+                    a.append(load_img(im['src'], image_size))
+                    b.append(labels_map[im['class']])
+            else:
+                for im in x[0:4]:
+                    a.append(load_img(im['src'], image_size))
+                    b.append(labels_map[im['class']])
 
             if i == n:
                 yield (np.array(a), np.array(b))
@@ -99,36 +75,26 @@ def train():
 
 
 def test():
-    global epoch
     test_images = [x for y in test_dataset.images for x in y]
     a = []
     b = []
     for x in test_images:
-        a.append(load_img(x['src'], image_size, False, False))
+        a.append(load_img(x['src'], image_size, False))
         b.append(labels_map[x['class']])
-    test1_images = [x for y in test1_dataset.images for x in y]
-    c = []
-    d = []
-    for x in test1_images:
-        c.append(load_img(x['src'], image_size, False, False))
-        d.append(labels_map[x['class']])
     while True:
-        if epoch < 150:
-            yield (np.array(c), np.array(d))
-        else:
-            yield (np.array(a), np.array(b))
+        yield (np.array(a), np.array(b))
 
 
 # Compile the model
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    0.00001,
-    decay_steps=1000,
+    0.001,
+    decay_steps=500,
     decay_rate=0.1,
     staircase=True
 )
 model.compile(
     optimizer=tf.keras.optimizers.Adam(lr_schedule),
-    loss=tfa.losses.TripletHardLoss(soft=True, margin=0.5)
+    loss=tfa.losses.TripletHardLoss(soft=True)
 )
 
 if FLAGS.model:
