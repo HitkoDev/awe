9f185f92ecb00a4d210895784772b323e72bb77d
HitkoDev <development@hitko.si>
2021-01-26 00:50:56 +0100

    do

--- heads/direct_normalize.py
+++ heads/direct_normalize.py
@@ -6,6 +6,6 @@ def head(endpoints, embedding_dim, is_training):
     endpoints['emb_raw'] = slim.fully_connected(
         endpoints['model_output'], embedding_dim, activation_fn=None,
         weights_initializer=tf.compat.v1.orthogonal_initializer(), scope='emb')
-    endpoints['emb'] = tf.nn.l2_normalize(endpoints['emb_raw'], -1, name="out_emb")
+    endpoints['emb'] = tf.nn.l2_normalize(tf.nn.dropout(endpoints['emb_raw'], 0.5), -1, name="out_emb")
 
     return endpoints
